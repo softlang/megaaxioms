386596e57710bc0dcca49e6c362c413f1fa820ed
Marcel <heinz@uni-koblenz.de>
2020-12-17 02:05:02 +0100

    fix test

--- test/models/emf.pl
+++ test/models/emf.pl
@@ -2,11 +2,4 @@
 :- include("../../test/solasote/ok_relation.pl").
 :- include("../../src/models/emf.pl").
 
-:- begin_tests(ci).
-
-test(failing) :-
-  1 == 2.
-
-:- end_tests(ci).
-
 :- run_tests.
