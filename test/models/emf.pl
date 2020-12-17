0e37de1e4b9d6f67933bf5f8296138bab327661d
Marcel <heinz@uni-koblenz.de>
2020-12-17 02:18:57 +0100

    check succeeds

--- test/models/emf.pl
+++ test/models/emf.pl
@@ -1,10 +1,3 @@
 :- include("../../test/solasote/ok_type.pl").
 :- include("../../test/solasote/ok_relation.pl").
 :- include("../../src/models/emf.pl").
-
-:- begin_tests(ci).
-
-test(failing) :-
-  1 == 2.
-
-:- end_tests(ci).
